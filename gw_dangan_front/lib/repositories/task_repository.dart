3814e62c433ac313977516f5d23b27ddf46134ad
ryuno3 <n.ryuno3@gmail.com>
2025-04-28 00:19:24 +0900

    忘れ物

--- gw_dangan_front/lib/repositories/task_repository.dart
+++ gw_dangan_front/lib/repositories/task_repository.dart
@@ -3,18 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:gw_dangan/models/tasks/task.dart';
 import 'package:http/http.dart' as http;
 
-// Future<List<Task>> fetchAllTasks() async {
-//   final response = await http.get(Uri.parse('http://localhost:8080/api/tasks'));
-//   if (response.statusCode == 200) {
-//     // JSONレスポンスをリストとしてデコード
-//     final List<dynamic> tasksJson = jsonDecode(response.body);
-//     // 各JSONオブジェクトをTaskオブジェクトに変換
-//     return tasksJson.map((json) => Task.fromJson(json)).toList();
-//   } else {
-//     throw Exception('Failed to load tasks');
-//   }
-// }
-
 class TaskRepository {
   final String baseUrl;
 

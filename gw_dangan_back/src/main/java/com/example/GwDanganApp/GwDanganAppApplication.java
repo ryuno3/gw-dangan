49fc4b37aa837f8281ced6f5e620a4c29ba592b8
ryuno3 <n.ryuno3@gmail.com>
2025-04-27 14:47:16 +0900

    ✨ javaセットアップ

---
+++ gw_dangan_back/src/main/java/com/example/GwDanganApp/GwDanganAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.GwDanganApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwDanganAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwDanganAppApplication.class, args);
+	}
+
+}
